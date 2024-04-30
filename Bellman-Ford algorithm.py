a9b4065dad0c4325bf1fd534b0f98cf38e7d8665
riddhidhanani <87064012+riddhidhanani@users.noreply.github.com>
2024-04-29 21:57:54 -0500

    Add files via upload

---
+++ Bellman-Ford algorithm.py
@@ -0,0 +1,49 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices  # Total number of vertices in the graph
+        self.graph = []  # Array of edges
+
+    # Add edges
+    def add_edge(self, s, d, w):
+        self.graph.append([s, d, w])
+
+    # Print the solution
+    def print_solution(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(self.V):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+    # The main function that finds shortest distances from src to all other vertices using Bellman-Ford algorithm
+    def bellman_ford(self, src):
+        # Step 1: Initialize distances from src to all other vertices as INFINITE
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+
+        # Step 2: Relax all edges |V| - 1 times. A simple shortest path from src to any other vertex can have at-most |V| - 1 edges
+        for _ in range(self.V - 1):
+            for s, d, w in self.graph:
+                if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+
+        # Step 3: Check for negative-weight cycles. The above step guarantees shortest distances if graph doesn't contain negative weight cycle.
+        # If we get a shorter path, then there is a cycle.
+        for s, d, w in self.graph:
+            if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                print("Graph contains negative weight cycle")
+                return
+
+        # No negative weight cycle found! Print the distance and predecessor array
+        self.print_solution(dist)
+
+# Example usage
+g = Graph(5)
+g.add_edge(0, 1, -1)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 3)
+g.add_edge(1, 3, 2)
+g.add_edge(1, 4, 2)
+g.add_edge(3, 2, 5)
+g.add_edge(3, 1, 1)
+g.add_edge(4, 3, -3)
+
+g.bellman_ford(0) 
